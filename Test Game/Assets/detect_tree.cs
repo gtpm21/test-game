acf73a1fdf01f6183e3a7015fea7b1e9ae8c2e9a
Fivos Vaxvanoglou <fivos.vaxe@gmail.com>
2022-10-07 00:15:48 +0300

    fixed tree-axe collision

--- Test Game/Assets/detect_tree.cs
+++ Test Game/Assets/detect_tree.cs
@@ -6,22 +6,29 @@ public class detect_tree : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject axe;
-    
-    private void OnTriggerEnter(Collider col)
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private FixedJoystick js;
+    Collider m_Collider;
+    bool triggered = false;
+    Collider other;
+
+    private void OnTriggerStay(Collider other)
     {
-        Debug.Log("Collision!");
-        if (col.gameObject.CompareTag("tree"))
+        if (other.gameObject.CompareTag("tree") && rb.velocity.magnitude == 0 && js.Horizontal == 0 && js.Vertical == 0)
         {
+            triggered = true;
+            this.other = other;
+            Debug.Log("Collision!");
             animator.SetBool("tree_detected", true);
             axe.tag = "axe";
         }
     }
 
-    private void OnTriggerExit(Collider col)
+    private void OnTriggerExit(Collider other)
     {
-        Debug.Log("No Collision!");
-        if (col.gameObject.CompareTag("tree"))
+        if (other.gameObject.CompareTag("tree"))
         {
+            Debug.Log("No Collision!");
             animator.SetBool("tree_detected", false);
             axe.tag = "Untagged";
         }
@@ -30,12 +37,17 @@ public class detect_tree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (triggered && !other.gameObject.activeInHierarchy)
+        {
+            Debug.Log("No Collision!");
+            animator.SetBool("tree_detected", false);
+            axe.tag = "Untagged";
+        }
     }
 }
