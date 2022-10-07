12b5ec7ec08f42ec3ca97a7116c6e41cee441c07
Fivos Vaxvanoglou <fivos.vaxe@gmail.com>
2022-10-07 15:50:06 +0300

    minor adjustments to collision of pickups

--- Test Game/Assets/Scripts/ItemPickup.cs
+++ Test Game/Assets/Scripts/ItemPickup.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
+    public int pickupNumber = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class ItemPickup : MonoBehaviour
     {
         if(col.gameObject.tag == "pickup")
         {
+            pickupNumber++;
             Destroy(col.gameObject);
         }
     }
