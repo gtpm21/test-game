612e761d991d6bf9d757ff5aa493d45e89bdb00c
Fivos Vaxvanoglou <fivos.vaxe@gmail.com>
2022-10-07 15:40:24 +0300

    added item pickup through force
    *still some bugs

---
+++ Test Game/Assets/Scripts/MagnetPickup.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetPickup : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float speed;
+    public float force = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "pickup")
+        {
+            Transform pickup = other.transform;
+            m_Rigidbody = other.attachedRigidbody;
+            m_Rigidbody.AddForce((transform.position - other.transform.position).normalized * force);
+
+
+            //pickup.position = Vector3.MoveTowards(pickup.position, transform.position, speed * Time.deltaTime);
+        }
+    }
+}
