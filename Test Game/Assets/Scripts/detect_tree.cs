e9e31ac2a382e4776eacbe96783bacc2c9c5a058
Fivos Vaxvanoglou <fivos.vaxe@gmail.com>
2022-10-08 01:44:34 +0300

    some adjustments

--- Test Game/Assets/Scripts/detect_tree.cs
+++ Test Game/Assets/Scripts/detect_tree.cs
@@ -6,14 +6,13 @@ public class detect_tree : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject axe;
-    [SerializeField] private Rigidbody rb;
     [SerializeField] private FixedJoystick js;
     bool triggered = false;
     Collider other;
 
     private void OnTriggerStay(Collider other)
     {
-        if ((other.gameObject.CompareTag("green_tree") || other.gameObject.CompareTag("pink_tree") || other.gameObject.CompareTag("cyan_tree")) && rb.velocity.magnitude == 0 && js.Horizontal == 0 && js.Vertical == 0)
+        if ((other.gameObject.CompareTag("green_tree") || other.gameObject.CompareTag("pink_tree") || other.gameObject.CompareTag("cyan_tree")) && js.Horizontal == 0 && js.Vertical == 0)
         {
             triggered = true;
             this.other = other;
