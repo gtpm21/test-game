30d8ff7a9318a82ceb15897b90b93c4f22a8cb40
Fivos Vaxvanoglou <fivos.vaxe@gmail.com>
2022-10-08 04:43:18 +0300

    added basic procedural level generation

---
+++ Test Game/Assets/Scripts/DistanceZones.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceZones : MonoBehaviour
+{
+    public int zPos = 100;
+}
---
+++ Test Game/Assets/Scripts/GenerateZone.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateZone : MonoBehaviour
+{
+    public GameObject zone;
+    public GameObject[] prefab;
+    public GameObject holder;
+    public int numberOfTrees = 120;
+    public int zPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        zPos = holder.GetComponent<DistanceZones>().zPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            Instantiate(zone, new Vector3(0, 0, zPos), Quaternion.identity);
+
+            if (zPos == 100) 
+            {
+                for (int i = 0; i <= numberOfTrees; i++) 
+                {
+                    var position = new Vector3(Random.Range(-9f, 9f), 0, Random.Range(zPos - 50, zPos + 50));
+                    Instantiate(prefab[0], position, Quaternion.identity);
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= numberOfTrees; i++)
+                {
+                    var position = new Vector3(Random.Range(-9f, 9f), 0, Random.Range(zPos - 50, zPos + 50));
+                    Instantiate(prefab[1], position, Quaternion.identity);
+                }
+            }
+            holder.GetComponent<DistanceZones>().zPos += 100;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
