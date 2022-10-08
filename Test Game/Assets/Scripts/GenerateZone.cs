16c374977c367377a92bd5610ebc560c30996033
Fivos Vaxvanoglou <fivos.vaxe@gmail.com>
2022-10-08 18:25:50 +0300

    added some props to starting zone

--- Test Game/Assets/Scripts/GenerateZone.cs
+++ Test Game/Assets/Scripts/GenerateZone.cs
@@ -7,7 +7,7 @@ public class GenerateZone : MonoBehaviour
     public GameObject zone;
     public GameObject[] prefab;
     public GameObject holder;
-    public int numberOfTrees = 120;
+    public int numberOfTrees = 150;
     public int zPos;
 
     // Start is called before the first frame update
@@ -27,14 +27,14 @@ public class GenerateZone : MonoBehaviour
     {
         if (other.gameObject.tag == "player")
         {
-            Instantiate(zone, new Vector3(0, 0, zPos), Quaternion.identity);
+            GameObject newZone = Instantiate(zone, new Vector3(0, 0, zPos), Quaternion.identity);
 
             if (zPos == 100) 
             {
                 for (int i = 0; i <= numberOfTrees; i++) 
                 {
                     var position = new Vector3(Random.Range(-9f, 9f), 0, Random.Range(zPos - 50, zPos + 50));
-                    Instantiate(prefab[0], position, Quaternion.identity);
+                    Instantiate(prefab[0], position, Quaternion.identity, newZone.transform);
                 }
             }
             else
@@ -42,7 +42,7 @@ public class GenerateZone : MonoBehaviour
                 for (int i = 0; i <= numberOfTrees; i++)
                 {
                     var position = new Vector3(Random.Range(-9f, 9f), 0, Random.Range(zPos - 50, zPos + 50));
-                    Instantiate(prefab[1], position, Quaternion.identity);
+                    Instantiate(prefab[1], position, Quaternion.identity, newZone.transform);
                 }
             }
             holder.GetComponent<DistanceZones>().zPos += 100;
